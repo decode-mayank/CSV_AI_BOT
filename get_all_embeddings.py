9e17ab3273a0519a4fd98125518fba7cd7826c76
Andrea Bency <andreabency@bitcot.com>
2023-02-13 11:37:58 +0530

    Added Interactive conversation and remembering the conversation

--- get_all_embeddings.py
+++ get_all_embeddings.py
@@ -7,6 +7,6 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 # df = pd.read_csv('scrape_data_with_syed.xlsx')
-df = pd.read_excel("3Categories(sleepApnea,sleepHealth,snoring).xlsx",sheet_name="Testing")
+df = pd.read_excel("embeddings_2.xlsx",sheet_name="embeddings")
 df['embedding'] = df['completion'].apply(lambda x: get_embedding(x, engine = 'text-embedding-ada-002'))
 df.to_csv('resmed_embeddings.csv')
