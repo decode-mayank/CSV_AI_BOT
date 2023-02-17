812fc50a316724e46f29c6c2027df78df7b971d3
Andrea Bency <andreabency@bitcot.com>
2023-02-17 17:51:54 +0530

    remembering the previous conversation

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def get_embedding(model, text):
 
 
 # Save embedding vector of the input
-def resmed_chatbot(input_text, history):
+def resmed_chatbot(input_text, inputs):
     input_embedding_vector = get_embedding(my_model, input_text)
 
     # Calculate similarity between the input and "facts" from companies_embeddings.csv file which we created before
@@ -32,20 +32,22 @@ def resmed_chatbot(input_text, history):
     df['embedding'] = df['embedding'].apply(eval).apply(np.array)
     df['similarity'] = df['embedding'].apply(lambda x: cosine_similarity(x, input_embedding_vector))
 
-    #print(df["similarity"])
     # Find the highest similarity value in the dataframe column 'similarity'
     highest_similarity = df['similarity'].max()
 
     # If the highest similarity value is equal or higher than 0.8 then print the 'completion' with the highest similarity
     if highest_similarity >= 0.8:
         fact_with_highest_similarity = df.loc[df['similarity'] == highest_similarity, 'completion']
-        return fact_with_highest_similarity.iloc[0]
+        fact = fact_with_highest_similarity.iloc[0]
+        print(fact)
+        outputs.append(fact)
         
     # Else pass input to the OpenAI Completions endpoint
     else:
         prompt = input_text
-        if len(history) > 0:
-            last_input, last_output = history[-1]
+        if len(inputs) > 0:
+            last_input = inputs[-1]
+            last_output = outputs[-1]
             prompt = f"{input_text} (based on my previous question: {last_input}, and your previous answer: {last_output})"
         response = openai.Completion.create(
             model="text-davinci-003",
@@ -53,28 +55,12 @@ def resmed_chatbot(input_text, history):
             max_tokens=200,
             temperature=0,
         )
-        output = response.choices[0].text
-        #history.append((input_text, output))
-        return output
+        content = response['choices'][0]['text'].replace('\n', '')
+        print(content)
 
-def chatgpt_clone(input, history):
-    history = history or []
-    s = list(sum(history, ()))
-    s.append(input)
-    inp = ' '.join(s)
-    output = resmed_chatbot(input, history)
-    history.append((input, output))
-    return history, history
-
-block = gr.Blocks()
-
-with block:
-    gr.Markdown("""<h1><center>Resmed Chatbot</center></h1>""")
-    chatbot = gr.Chatbot()
-    message = gr.Textbox(placeholder="Text Here")
-    state = gr.State()
-    submit = gr.Button("SEND")
-    submit.click(chatgpt_clone, inputs=[message, state], outputs=[chatbot, state])
+while True:
+    input_text = input("user:")
+    inputs.append(input_text)
+    resmed_chatbot(input_text, inputs)
 
 
-block.launch(debug=True)
