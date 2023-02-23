048aa64bbc8edc517ce7d6609d063994ae501191
aarti <aarti@bitcot.com>
2023-02-23 16:42:33 +0530

    Scraped all category, missing text and hyperlink

--- 1scrape_knowledge_hub.py
+++ 1scrape_knowledge_hub.py
@@ -1,6 +1,9 @@
 import scrapy
+from scrapy.crawler import CrawlerProcess
+
 
 class ResmedSpider(scrapy.Spider):
+
     name = 'resmed'
     allowed_domains = ['www.resmed.com.au']
     start_urls = 'https://www.resmed.com.au/knowledge-hub'
@@ -21,19 +24,21 @@ class ResmedSpider(scrapy.Spider):
 
 
     def parse_answer_page(self, response):
-        category=response.css(".brd-crm::text").extract_first()
-        head = response.css('.section-right-heading h1::text').extract_first()
-        sub_head = response.css('.section-right-sub-heading h3::text').extract_first()
-        content = response.css('.section-content-rt p::text').extract()
-        sub_con = response.css('.section-content-rt p a::text').extract_first()
-        content_li = response.css('.section-content-rt ol li strong::text').extract()
-        data = " ".join(content_li)
-        ref = response.css('.section-content-rt ol li::text').extract()
-        refrence = ' '.join(ref)
-        completion = sub_head+ '\n' + "\n".join(content) + '\n' + sub_con +'\n' + data + '\n' + refrence
-        product_dict = {
-            'prompt': head,
-            'completion': completion,
-            'category': category
-        }
-        yield product_dict
+        try:
+            head = response.css('.section-right-heading h1::text').extract_first()
+            sub_head = response.css('.section-right-sub-heading h3::text').extract_first()
+            sub_heading = sub_head if sub_head is not None else ''
+            content_p = response.css(
+                '.section-content-rt p::text, .section-content-rt ul li::text, .section-content-rt p a::text, .section-content-rt ol li strong::text, .section-content-rt p strong::text, .section-content-rt p strong span::text, .section-content-rt ol li::text, .section-content-rt p a::attr(href),'
+                ' .section-content-rt ol li span::text, .section-content-rt ol li a::text, .section-content-rt ol li a::attr(href), .section-content-rt p em::text, .section-content-rt li strong::text, .section-content-rt li h4::text, .section-content-rt h4::text, .section-content-rt h3::text, .section-content-rt ul li a::text, .section-content-rt ul li a::attr(href), .section-content-rt span::text, .section-content-rt span::text, .section-content-rt a::text, .section-content-rt a::attr(href)').extract()
+            content = "\n".join(content_p) if content_p != [] else ''
+            completion = sub_heading+ '\n' + content
+            product_dict = {
+                'type': response.css('.brd-crm::text').extract_first(),
+                'prompt': head,
+                'completion': completion,
+                'url': response.url
+            }
+            yield product_dict
+        except:
+            pass
