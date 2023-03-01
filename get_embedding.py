5af633f5f1fe15368472fe988922afabb800fc11
Andrea Bency <andreabency@bitcot.com>
2023-03-01 15:46:53 +0530

    Added code and file for finding category

---
+++ get_embedding.py
@@ -0,0 +1,12 @@
+import openai
+from openai.embeddings_utils import get_embedding
+import pandas as pd
+import os
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+# df = pd.read_csv('scrape_data_with_syed.xlsx')
+df = pd.read_excel("resmed.xlsx",sheet_name="Sheet1")
+df['embedding'] = df['prompt'].apply(lambda x: get_embedding(x, engine = 'text-embedding-ada-002'))
+df.to_csv('product_embeddings.csv')
