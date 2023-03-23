3fedcfb64ce4dd5821c20f62b8cd36511555394f
Syed-Vtex <syed.mujeeb@vtex.com.br>
2023-03-23 15:01:47 +0530

    Use case insensitive search for searching products + improvements

--- products.py
+++ products.py
@@ -5,7 +5,7 @@ import openai
 import psycopg2
 import sqlparse
 from dotenv import load_dotenv
-
+from debug_utils import debug_steps
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -16,11 +16,17 @@ EMBEDDING_MODEL = "text-embedding-ada-002"
 DEFAULT_RESPONSE_WHEN_NO_QUERY_FOUND= "Please rephrase your query"
 
 def generate_prompt(text,instruction):
-    return f"Given an input question, respond with syntactically correct PostgreSQL. Be creative but the query must be correct. Only use table called product. The product table has columns: category (character varying), sku (character varying), product (character varying), description (character varying), price (character varying), breadcrumb (character varying), product_url (character varying), money_back (BOOLEAN), rating (FLOAT), total_reviews (INTEGER), tags(character varying). Give a Select query for product, product_url and price, where the tags matches to the input question.{text}. Format the query in the correct format.{instruction}"
+    return f"Given an input question, respond with syntactically correct PostgreSQL. Be creative but the query must be correct. Only use table called product. The product table has columns: category (character varying), sku (character varying), product (character varying), description (character varying), price (character varying), breadcrumb (character varying), product_url (character varying), money_back (BOOLEAN), rating (FLOAT), total_reviews (INTEGER), tags(character varying). Give a Select query for product, product_url and price, where the tags matches to the input question.{text}. Format the query in the correct format.Use case insensitive search for tags column.{instruction}"
 
-def execute_query(conn,query):
+def execute_query(conn,row,response,fn_name):
+    query = response['choices'][0]['text']
+    start = "SELECT"
+    end = ";"
+    start_pos = query.find(start)
+    end_pos = query.find(end)
+    query = query[(start_pos-1):(end_pos+1)].strip()
     sqlparse.format(query, reindent=True, keyword_case='upper')
-    debug(query)
+    debug_steps(row,f"DB Query from {fn_name}",query)
     cur = conn.cursor()
     if(query):
         cur.execute(query)
@@ -28,10 +34,6 @@ def execute_query(conn,query):
     else:
         return DEFAULT_RESPONSE_WHEN_NO_QUERY_FOUND
 
-def debug(msg):
-    verbose=os.getenv('VERBOSE')
-    if verbose=="True":
-        print(msg)  
 
 conn = psycopg2.connect(
         host="localhost",
@@ -39,7 +41,7 @@ conn = psycopg2.connect(
         user=os.getenv('DB_USERNAME'),
         password=os.getenv('DB_PASSWORD'))
 
-def product(text):
+def product(row,text):
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=generate_prompt(text,"Use Order_by command to order the rating in Descending order and list top 3 items"),
@@ -50,16 +52,10 @@ def product(text):
         presence_penalty=0.0
     )
 
-    query = response['choices'][0]['text']
-    start = "SELECT"
-    end = ";"
-    start_pos = query.find(start)
-    end_pos = query.find(end)
-    query = query[(start_pos-1):(end_pos+1)].strip()
-    output = execute_query(conn,query)
+    output = execute_query(conn,row,response,product.__name__)
     return(output)
 
-def other_products(text):
+def other_products(row,text):
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=generate_prompt(text,"Use Order_by command to order the rating in Ascending order and list top 3 items"),
@@ -70,17 +66,11 @@ def other_products(text):
         presence_penalty=0.0
     )
 
-    query = response['choices'][0]['text']
-    start = "SELECT"
-    end = ";"
-    start_pos = query.find(start)
-    end_pos = query.find(end)
-    query = query[(start_pos-1):(end_pos+1)].strip()
-    output = execute_query(conn,query)
+    output = execute_query(conn,row,response,other_products.__name__)
     return(output)
 
 
-def cheap_products(text):
+def cheap_products(row,text):
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=generate_prompt(text,"Use Order_by command to order the price in Ascending order and list top 1 items"),
@@ -90,20 +80,14 @@ def cheap_products(text):
         frequency_penalty=0.0,
         presence_penalty=0.0
     )
-    query = response['choices'][0]['text']
-    start = "SELECT"
-    end = ";"
-    start_pos = query.find(start)
-    end_pos = query.find(end)
-    query = query[(start_pos-1):(end_pos+1)].strip()
-    output = execute_query(conn,query)
+    output = execute_query(conn,row,response,cheap_products.__name__)
     return(output)
 
 
-def general_product(text):
+def general_product(row,text):
     response = openai.Completion.create(
         model="text-davinci-003",
-        prompt=f"Given an input question, respond with syntactically correct PostgreSQL. Be creative but the query must be correct. Only use table called product. The product table has columns: category (character varying), sku (character varying), product (character varying), description (character varying), price (character varying), breadcrumb (character varying), product_url (character varying), money_back (BOOLEAN), rating (FLOAT), total_reviews (INTEGER), tags (character varying). Understand the user input correctly and create a SQL query. Give a Select query for product, product_url and price.{text}. Format the query in the correct format. Suggest any 2 product as per user Query. Write an SQL query that retrieves data from table based on a specified condition. The query should return specific columns and have proper formatting and syntax. Use tags in condition if there is any product or category mentioned in user input and if Multiple conditions go with OR command.",        
+        prompt=f"Given an input question, respond with syntactically correct PostgreSQL. Be creative but the query must be correct. Only use table called product. The product table has columns: category (character varying), sku (character varying), product (character varying), description (character varying), price (character varying), breadcrumb (character varying), product_url (character varying), money_back (BOOLEAN), rating (FLOAT), total_reviews (INTEGER), tags (character varying). Understand the user input correctly and create a SQL query. Give a Select query for product, product_url and price.{text}. Format the query in the correct format. Suggest any 2 product as per user Query. Write an SQL query that retrieves data from table based on a specified condition. The query should return specific columns and have proper formatting and syntax. Use tags in condition if there is any product or category mentioned in user input and if Multiple conditions go with OR command. Use case insensitive search for tags column",        
         temperature=0.3,
         max_tokens=60,
         top_p=1.0,
@@ -116,5 +100,6 @@ def general_product(text):
     start_pos = query.find(start)
     end_pos = query.find(end)
     query = query[(start_pos-1):(end_pos+1)].strip()
-    output = execute_query(conn,query)
+
+    output = execute_query(conn,row,response,general_product.__name__)
     return(output)
--- utils_backup.py
+++ utils_backup.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 import time
 import os
-import csv 
 
 import openai
 from openai.embeddings_utils import cosine_similarity
@@ -16,15 +15,13 @@ from tenacity import (
     wait_random_exponential,
 )  # for exponential backoff
 
-from colors import pr_cyan,pr_bot_response
-from debug_utils import debug_steps,debug, debug_attribute
+from colors import pr_light_purple,pr_yellow,pr_pink,pr_cyan,pr_bot_response,pr_green
 
 # Insert your API key
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 my_model = 'text-embedding-ada-002'
 
-DEBUG_CSV = "debug.csv"
 
 RESPONSE_FOR_INVALID_QUERY = "I am a Resmed chatbot, I can't help with that"
 
@@ -39,7 +36,18 @@ NO = "No"
 VERBOSE = os.getenv('VERBOSE')
 EXPECTED_SIMILARITY = 0.85
 
-
+def debug_steps(msg,attribute=""):
+    if VERBOSE=="True":
+        pr_green(f"[DEBUG] - {msg} {attribute}") 
+        
+def debug(msg):
+    if VERBOSE=="True":
+        pr_pink(f"[DEBUG] - {msg}")  
+    
+def debug_attribute(attribute,value):
+    if VERBOSE=="True":
+        pr_light_purple(attribute,end="")
+        pr_yellow(value,end="\n")
        
 # Get db connections
 def get_db_connection():
@@ -119,8 +127,8 @@ def identify_answer(user_input, bot_response):
     response = openai.Completion.create(
     model="text-davinci-003",
     prompt=f"The following is a conversation with an AI assistant. The assistant only answers {YES} or {NO}\n{YES} If the question and answer make sense otherwise say {NO} \nHuman:Q:{user_input},A:{bot_response}\nAI:",
-    temperature=0.5,
-    max_tokens=10,
+    temperature=0,
+    max_tokens=1,
     stop=["Human:", "AI:"]
     )
     return response.choices[0].text.strip()
@@ -130,7 +138,7 @@ def find_what_user_expects(user_input):
     response = openai.Completion.create(
     model="text-davinci-003",
     prompt=f"Find what user expects from the chatbot system Expected Responses are {GENERAL_QUERY},{SYMPTOM_QUERY},{PRODUCT_QUERY},{PROGRAM_QUERY} \nHuman: I forget a lot and not able to concentrate \nAI:{SYMPTOM_QUERY}\nHuman: Does resmed provide CPAP Products\nAI:{GENERAL_QUERY}\nHuman: I have Mood disruptions, especially anxiety, depression and irritability\nAI:{SYMPTOM_QUERY}\nHuman: How many hours should i sleep daily\nAI:{GENERAL_QUERY}\nHuman:What is the price of CPAP mask\nAI:{PRODUCT_QUERY}\nHuman:Write a program\nAI:{PROGRAM_QUERY}\nHuman:{user_input}",
-    temperature=0.5,
+    temperature=0,
     max_tokens=10,
     top_p=1,
     stop=[" Human:", " AI:"]
@@ -151,18 +159,16 @@ def find_whether_user_query_is_valid(user_input):
 def show_products(output):
     prod_response = '\n'
     if(len(output)>0):
-        items = output[0]
-        debug_attribute("DB Output",output)
-        if(len(items)==3):
-            pr_cyan("Here are some products, which matches your search")
-            for prod, url,price in output:
-                products = prod + " - " + url + " - $" + str(price)
-                pr_cyan(products)
-                prod_response = prod_response + "\n" + products
+        pr_cyan("Here are some products, which matches your search")
+    debug_attribute("DB Output",output)
+    for prod, url,price in output:
+        products = prod + " - " + url + " - $" + str(price)
+        pr_cyan(products)
+        prod_response = prod_response + "\n" + products
     return prod_response
 
-def product_query(row,user_input, message_log, bot_response):
-    output = general_product(row,user_input)
+def product_query(user_input, message_log, bot_response):
+    output = general_product(user_input)
     if len(output) == 0:
         bot_response += call_chat_completion_api(message_log)
     else:
@@ -170,16 +176,6 @@ def product_query(row,user_input, message_log, bot_response):
     return bot_response
 
 def resmed_chatbot(user_input,message_log,db=True):
-    
-    MODE = 'w'
-    fields = ["user_input","bot_response","level1","level2","level3","level4","level5","level6","db_query"]
-    row = []
-    row.append(user_input)
-    MAX_COLUMNS = len(fields)
-    
-    if os.path.exists(DEBUG_CSV):
-        MODE='a'
-    
     # Append user_input 
     message_log.append({"role": "user", "content": user_input})
     
@@ -208,20 +204,20 @@ def resmed_chatbot(user_input,message_log,db=True):
         df = pd.read_csv('resmed_embeddings_final.csv')
         
         if (len(user_input.split(' '))==1):
-            debug_steps(row,"Level 1 - Got a word from user - Prepend What is")
+            debug_steps("Level 1 - Got a word from user - Prepend What is")
             # If user types single word input then system gets confused so adding what is as a prefix
             user_input= f"What is {user_input}"
             
         query_type = find_what_user_expects(user_input)
-        debug_steps(row,"Level 1 - Find what user expects -",query_type)
+        debug_steps("Level 1 - Find what user expects -",query_type)
 
         debug_attribute("query_type - ",query_type)
         
         if(PROGRAM_QUERY in query_type):
-            debug_steps(row,"Level 2 - Found program query - ",query_type)
+            debug_steps("Level 2 - Found program query - ",query_type)
             pr_bot_response(bot_response)
         elif(GENERAL_QUERY not in query_type):
-            debug_steps(row,"Level 2 - It is not a general query -",query_type)
+            debug_steps("Level 2 - It is not a general query -",query_type)
             if 'similarity' in df.columns:
                 df['embedding'] = df['embedding'].apply(np.array)
             else:
@@ -234,12 +230,12 @@ def resmed_chatbot(user_input,message_log,db=True):
             debug_attribute("similarity - ",highest_similarity)
                     
             if(highest_similarity >= EXPECTED_SIMILARITY and query_type!=""):
-                debug_steps(row,"Level 3 - Found hightest similarity",highest_similarity)
+                debug_steps("Level 3 - Found hightest similarity",highest_similarity)
                 probability = highest_similarity
                 fact_with_highest_similarity = df.loc[df['similarity'] == highest_similarity, 'completion']
                 db_input_or_bot_response = fact_with_highest_similarity.iloc[0]
                 show_embedding_answer_to_user = identify_answer(user_input, db_input_or_bot_response)
-                debug_steps(row,"Level 4 - Identify embedded output is useful or not - ",show_embedding_answer_to_user)
+                debug_steps("Level 4 - Identify embedded output is useful or not - ",show_embedding_answer_to_user)
                 debug_attribute("embedded_bot_response - ",db_input_or_bot_response)
                 debug_attribute("show_embedding_answer_to_user - ",show_embedding_answer_to_user)
                 
@@ -249,58 +245,58 @@ def resmed_chatbot(user_input,message_log,db=True):
                 else:
                     source = ""
                     if "others" == db_input_or_bot_response:
-                        debug_steps(row,"Level 5 - Symptoms are common")
+                        debug_steps("Level 5 - Symptoms are common")
                         bot_response = "Your symptoms are more common to define the exact syndrome. can you please provide more detail:"
                         pr_bot_response(bot_response)
                     else:
                         found_symptom = db_input_or_bot_response=="Sleep Apnea" or db_input_or_bot_response=="Insomnia" or db_input_or_bot_response=="Snoring"
                         if (SYMPTOM_QUERY in query_type and found_symptom) or PRODUCT_QUERY in query_type:
                             if(found_symptom):
-                                debug_steps(row,"Level 5 - Symptoms found")
+                                debug_steps("Level 5 - Symptoms found")
                                 if db_input_or_bot_response in user_input:
-                                    debug_steps(row,"Level 6 - Suggest products")
-                                    output = general_product(row,user_input)
+                                    debug_steps("Level 6 - Suggest products")
+                                    output = general_product(user_input)
                                     print("Here are some products, which matches your search")
                                     bot_response = show_products(output)  
                                 else:
-                                    debug_steps(row,"Level 6 - Inform sleep disorder and suggest product")
+                                    debug_steps("Level 6 - Inform sleep disorder and suggest product")
                                     MSG = f"This appears to be a condition called {db_input_or_bot_response}.It is a fairly common condition, which can be addressed. We recommend you take an assessment and also speak to a Doctor."
                                     pr_bot_response(MSG)
                                     SLEEP_ASSESSMENT_INFO="For more information please visit'\033]8;;https://info.resmed.co.in/free-sleep-assessment\aSleep Assessment\033]8;;\a'"
                                     pr_cyan(SLEEP_ASSESSMENT_INFO)
                                     bot_response= f"{MSG}\n{SLEEP_ASSESSMENT_INFO}"
-                                    output = product(row,db_input_or_bot_response)
+                                    output = product(db_input_or_bot_response)
                                     bot_response += show_products(output)
                             elif "cheap" in user_input or "cheapest" in user_input:
-                                debug_steps(row,"Level 6 - Suggest cheap/ cheapest products")
+                                debug_steps("Level 6 - Suggest cheap/ cheapest products")
                                 probability = 0
                                 if len(outputs)==0:
-                                    output = cheap_products(row,user_input)
+                                    output = cheap_products(user_input)
                                 else:
-                                    output = cheap_products(row,outputs[-1])
-                                show_products(output)
-                            elif "Product" == db_input_or_bot_response:
-                                debug_steps(row,"Level 6 - Suggest products")
-                                output = other_products(row,outputs[-1])
+                                    output = cheap_products(outputs[-1])
+                                for prod, url, price in output:
+                                    bot_response = prod + " - " + url + " - $" + str(price)
+                                    pr_cyan(f"Cheapest option: {bot_response}")
+                            elif "product" == bot_response:
+                                debug_steps("Level 6 - Suggest products")
+                                output = other_products(outputs[-1])
                                 bot_response += show_products(output) 
                             else:
-                                # debug_steps(f"We are in else part,query_type is {query_type}, bot_response is {bot_response}")
-                                debug_steps(row,"Level 6 - Suggest products")
-                                bot_response = product_query(row,user_input, message_log, bot_response)
-                    outputs.append(bot_response)  
+                                debug_steps(f"We are in else part,query_type is {query_type}, bot_response is {bot_response}")
+                                debug_steps("Level 6 - Suggest products")
+                                bot_response = product_query(user_input, message_log, bot_response)
+                        outputs.append(bot_response)
             elif PRODUCT_QUERY in query_type:
-                debug_steps(row,"Level 3 - Search product")
-                # Let's add dummy rows to fill 4,5,6 levels
-                row.extend(["","",""])
+                debug_steps("Level 3 - Search product")
                 source=""
-                bot_response = product_query(row,user_input, message_log, bot_response)                  
+                bot_response = product_query(user_input, message_log, bot_response)                  
             else:
-                debug_steps(row,"Level 3 - General query")
+                debug_steps("Level 3 - General query")
                 debug(f"It is a general query / similarity {highest_similarity*100} less than {EXPECTED_SIMILARITY*100}")
                 bot_response = call_chat_completion_api(message_log)
                 outputs.append(bot_response)
     else:
-        debug_steps(row,"Level 1 - Query not related to resmed")
+        debug_steps("Level 1 - Query not related to resmed")
         # Looks like user asked query which is not related to resmed
         bot_response=response
         pr_bot_response(bot_response)
@@ -329,25 +325,14 @@ def resmed_chatbot(user_input,message_log,db=True):
         debug("DB insert is disabled")
     debug(f"Response time in seconds - {response_time}")
     
-    if VERBOSE=="True":
-        debug(f"Writing the logs in {DEBUG_CSV}")
-        with open(DEBUG_CSV, MODE) as csvfile: 
-            # creating a csv writer object 
-            csvwriter = csv.writer(csvfile) 
-                
-            if MODE=='w':
-                # writing the fields 
-                csvwriter.writerow(fields) 
-                
-            row_length = len(row)
-            if(row_length!=MAX_COLUMNS-1):
-                dummy_rows_to_add = MAX_COLUMNS-row_length-2
-                row.extend(('-'*dummy_rows_to_add).split('-'))
-            # writing the data rows 
-            row.insert(1,bot_response)
-            csvwriter.writerows([row])
-    
     # Add the chatbot's response to the conversation history and print it to the console
     message_log.append({"role": "assistant", "content": response})
     
     return bot_response,message_log
+
+# Zero shot, One shot, Few shot - Here shot is an example which we share in prompt for auto completion
+# https://www.youtube.com/watch?v=v2gD8BHOaX4 - About Stop sequence
+
+
+# user_input="Syed"
+# print(f"Find what user expects from the chatbot system Expected Responses are {GENERAL_QUERY},{SYMPTOM_QUERY},{PRODUCT_QUERY},{PROGRAM_QUERY} \nHuman: I forget a lot and not able to concentrate \nAI:{SYMPTOM_QUERY}\nHuman: Does resmed provide CPAP Products\nAI:{GENERAL_QUERY}\nHuman: I have Mood disruptions, especially anxiety, depression and irritability\nAI:{SYMPTOM_QUERY}\nHuman: How many hours should i sleep daily\nAI:{GENERAL_QUERY}\nHuman:What is the price of CPAP mask\nAI:{PRODUCT_QUERY}\nHuman:Write a program\nAI:{PROGRAM_QUERY}\nHuman:{user_input}")
