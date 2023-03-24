509411716222033f2f3dd7c7c384a6459096cc2b
Kajol <kajolmehta@bitcot.com>
2023-03-24 15:22:39 +0530

    testcase for product query

--- test_product_queries.py
+++ test_product_queries.py
@@ -1,12 +1,12 @@
-from utils import find_what_user_expects, debug_attribute
+from utils import find_what_user_expects, debug_attribute, resmed_chatbot
 from constants import MESSAGE_LOG
 
 message_log = MESSAGE_LOG
   
 def test_product_query():
-    user_input = "can you suggest some snoring products"
-    product_response = find_what_user_expects(user_input)
+    user_input = "do you have any adapter?"
+    product_response = resmed_chatbot(user_input, message_log, False)
     debug_attribute("product_response",product_response)
-    assert product_response == "AI:Product query"
+    assert "\n\n" in product_response[0]
 
     
