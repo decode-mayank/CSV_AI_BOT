3c1eb74159108dcb37c6639bb1b0b6be0e42d9f9
Kajol <kajolmehta@bitcot.com>
2023-03-25 20:21:38 +0530

    Automate testcases

--- test_product_queries.py
+++ test_product_queries.py
@@ -1,12 +1,17 @@
 from utils import find_what_user_expects, debug_attribute, resmed_chatbot
 from constants import MESSAGE_LOG
+import pandas as pd
 
 message_log = MESSAGE_LOG
+df = pd.read_csv("testcases.csv")
   
-def test_product_query():
-    user_input = "do you have any adapter?"
+def test_product_query(user_input):
     product_response = resmed_chatbot(user_input, message_log, False)
     debug_attribute("product_response",product_response)
-    assert "\n\n" in product_response[0]
-
-    
+    try:
+        assert "\n\n" in product_response[0]
+        return "Pass"
+    except AssertionError:
+        return "Fail"
+df["output"] = df.apply(lambda row: test_product_query(row["User Input"]), axis=1)
+df.to_csv("testcases.csv", index=False)
