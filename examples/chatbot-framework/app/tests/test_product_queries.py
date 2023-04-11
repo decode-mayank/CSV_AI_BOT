ab5aa9cd59efcd712c4d7acbb557608f9b4299c8
Kajol <kajolmehta@bitcot.com>
2023-04-11 17:59:07 +0530

    resolve conflicts

--- examples/chatbot-framework/app/tests/test_product_queries.py
+++ examples/chatbot-framework/app/tests/test_product_queries.py
@@ -5,14 +5,10 @@ import pandas as pd
 message_log = MESSAGE_LOG
 df = pd.read_csv("testcases.csv")
   
-<<<<<<< HEAD:test_product_queries.py
-def test_product_query(user_input):
-    product_response = resmed_chatbot(user_input, message_log, False)
-=======
+
 def test_product_query():
     user_input = "do you have any adapter?"
     product_response = chatbot(user_input, message_log, False)
->>>>>>> 934cdc5649f227defede41c3540cc9c70ecace43:examples/chatbot-framework/app/tests/test_product_queries.py
     debug_attribute("product_response",product_response)
     try:
         assert "\n\n" in product_response[0]
