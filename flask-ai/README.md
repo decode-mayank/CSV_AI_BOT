46edfa960f5770b090abf5d872cb760d319ceb0a
Syed-Vtex <syed.mujeeb@vtex.com.br>
2023-04-21 17:09:15 +0530

    Create database url at runtime

--- flask-ai/README.md
+++ flask-ai/README.md
@@ -1,8 +1,5 @@
 Reference - https://rest-apis-flask.teclado.com/docs/first_rest_api/getting_set_up/
 
-# How to run server
-flask run
-
 # If migrations folder not exist in the project
 flask db init
 
@@ -13,6 +10,8 @@ flask db upgrade
 # If you already have migrations file in migrations folder then run below command
 flask db upgrade
 
+# How to run server
+flask run
 
 # Flask server run on default port 5000
 http://127.0.0.1:5000/
--- flask-ai/app.py
+++ flask-ai/app.py
@@ -13,8 +13,7 @@ from resources.routes import blp as ChatBotBlueprint
 load_dotenv()
 
 
-def create_app(db_url=None):
-
+def create_app():
     app = Flask(__name__)
     CORS(app)
     app.config["PROPAGATE_EXCEPTIONS"] = True
@@ -24,7 +23,7 @@ def create_app(db_url=None):
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url or os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{os.getenv("DATABASE_USER")}:{os.getenv("DATABASE_PASSWORD")}@{os.getenv("DATABASE_HOST")}:5432/{os.getenv("DATABASE_NAME")}'
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     migrate = Migrate(app, db)
